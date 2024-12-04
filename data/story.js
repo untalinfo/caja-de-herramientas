faa5346232b50c7b903ffd081a80f08bd89d4eea
oscar Info <oscar.giraldo@ingeneosas282.onmicrosoft.com>
2024-12-04 18:05:10 -0500

    upload files

---
+++ data/story.js
@@ -0,0 +1,503 @@
+var story = {
+ "docName": "Caja de herramientas",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Caja de herramientas",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "24:111",
+   "groupIndex": 0,
+   "title": "REA",
+   "image": "rea.png",
+   "index": 0,
+   "width": 1440,
+   "height": 1024,
+   "x": -4470,
+   "y": 797,
+   "awidth": 1440,
+   "aheight": 1024,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "btn watch",
+     "rect": {
+      "x": 1232,
+      "y": 503,
+      "width": 113,
+      "height": 28.30384063720703
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "logo alcaldia",
+     "rect": {
+      "x": 28,
+      "y": 7,
+      "width": 75,
+      "height": 57
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "83:465",
+   "groupIndex": 0,
+   "title": "STEAM+ desc",
+   "image": "steam+-desc.png",
+   "index": 1,
+   "width": 1440,
+   "height": 1406,
+   "x": -4470,
+   "y": 2025,
+   "awidth": 1440,
+   "aheight": 1406,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group 8083",
+     "rect": {
+      "x": 1136,
+      "y": 1301,
+      "width": 80.70877075195312,
+      "height": 23.03466796875
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "URL",
+       "srcPageIndex": 1,
+       "url": "https://crea-portaldemedios.siemens-stiftung.org/ia-generativa-aplicaciones-y-consideraciones-eticas-en-la-educacion-secundaria-103609",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "logo alcaldia",
+     "rect": {
+      "x": 28,
+      "y": 7,
+      "width": 75,
+      "height": 57
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "123:525",
+   "groupIndex": 0,
+   "title": "co-creacion",
+   "image": "co-creacion.png",
+   "index": 2,
+   "width": 1440,
+   "height": 1024,
+   "x": -2507,
+   "y": 797,
+   "awidth": 1440,
+   "aheight": 1024,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "logo alcaldia",
+     "rect": {
+      "x": 28,
+      "y": 7,
+      "width": 75,
+      "height": 57
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "btn watch",
+     "rect": {
+      "x": 1246,
+      "y": 373,
+      "width": 113,
+      "height": 28.30384063720703
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "btn watch",
+     "rect": {
+      "x": 1246,
+      "y": 523,
+      "width": 113,
+      "height": 28.30384063720703
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "147:317",
+   "groupIndex": 0,
+   "title": "competencias basicas",
+   "image": "competencias-basicas.png",
+   "index": 3,
+   "width": 1440,
+   "height": 1406,
+   "x": -2507,
+   "y": 2025,
+   "awidth": 1440,
+   "aheight": 1406,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "logo alcaldia",
+     "rect": {
+      "x": 28,
+      "y": 7,
+      "width": 75,
+      "height": 57
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "36:387",
+   "groupIndex": 0,
+   "title": "Formulario contacto",
+   "image": "formulario-contacto.png",
+   "index": 4,
+   "width": 468,
+   "height": 685,
+   "x": -1424,
+   "y": -347,
+   "awidth": 468,
+   "aheight": 685,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "overlay",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  },
+  {
+   "id": "1:2",
+   "groupIndex": 0,
+   "title": "Home",
+   "image": "home.png",
+   "index": 5,
+   "width": 1440,
+   "height": 1024,
+   "x": -846,
+   "y": -496,
+   "awidth": 1440,
+   "aheight": 1024,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "1",
+     "rect": {
+      "x": 907,
+      "y": 708,
+      "width": 295,
+      "height": 300
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "2",
+     "rect": {
+      "x": 577,
+      "y": 708,
+      "width": 295,
+      "height": 300
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "3",
+     "rect": {
+      "x": 239,
+      "y": 708,
+      "width": 295,
+      "height": 300
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Group 8098",
+     "rect": {
+      "x": 1273,
+      "y": 834,
+      "width": 91,
+      "height": 91
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "OVERLAY",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 4,
+       "pageType": "overlay",
+       "overlayCloseOnClickOutside": true,
+       "overlayPinType": "PAGE",
+       "overlayPinPagePositionType": "BOTTOM_RIGHT",
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "123:680",
+   "groupIndex": 0,
+   "title": "banco datos",
+   "image": "banco-datos.png",
+   "index": 6,
+   "width": 1440,
+   "height": 1024,
+   "x": -846,
+   "y": 797,
+   "awidth": 1440,
+   "aheight": 1024,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "logo alcaldia",
+     "rect": {
+      "x": 28,
+      "y": 7,
+      "width": 75,
+      "height": 57
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "147:535",
+   "groupIndex": 0,
+   "title": "Ondemand video",
+   "image": "ondemand-video.png",
+   "index": 7,
+   "width": 96,
+   "height": 96,
+   "x": 467,
+   "y": 36,
+   "awidth": 96,
+   "aheight": 96,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 5,
+ "totalImages": 8,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
